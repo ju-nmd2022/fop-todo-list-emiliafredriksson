ba0a2280ae9394ba0d9ccf8996bc152c67660172
Emilia Fredriksson <frem22pu@student.ju.se>
2023-04-11 16:07:01 +0200

    adding chores

--- toDoList.js
+++ toDoList.js
@@ -2,6 +2,7 @@ const listElement = document.getElementById("list");
 const inputElement = document.getElementById("input");
 const addButtonElement = document.getElementById("add");
 const choresElement = document.getElementById("chores");
+const completedElement = document.getElementById("completed");
 
 addButtonElement.addEventListener("click", addChores);
 
@@ -10,6 +11,27 @@ function addChores() {
     // console.log(inputElement.value);
     const inputChore = document.createElement("div");
     inputChore.innerText = inputElement.value;
+    inputChore.classList.add("text");
     choresElement.appendChild(inputChore);
+
+    inputElement.value = "";
+
+    const doneButton = document.createElement("button");
+    doneButton.innerText = "✓";
+    choresElement.appendChild(doneButton);
+    doneButton.addEventListener("click", () => {
+      completedElement.appendChild(inputChore);
+      doneButton.remove();
+      completedElement.appendChild(deleteButton);
+    });
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "✕";
+    choresElement.appendChild(deleteButton);
+    deleteButton.addEventListener("click", () => {
+      inputChore.remove();
+      doneButton.remove();
+      deleteButton.remove();
+    });
   }
 }
