93bd1578bf1d18e8f9058dd7d4d428468f073a28
Emilia Fredriksson <frem22pu@student.ju.se>
2023-04-11 23:54:36 +0200

    local storage not going great

---
+++ to-do-list.js
@@ -0,0 +1,86 @@
+/* Code about local storage and JSON was written 
+with help/inspiration of Tyler Potts on Youtube and 
+Tamzin Cleggs code, some changes and adaption was made. 
+Link:https://www.youtube.com/watch?v=UG2LXxILAnM */
+
+const inputElement = document.getElementById("input");
+const addButtonElement = document.getElementById("add");
+const choresElement = document.getElementById("chores");
+
+// Array to store the chores
+const choresArray = [];
+console.log(choresArray);
+
+// function to get back the array from local storage
+function storage() {
+  const savedChores = localStorage.getItem("choresArray");
+
+  if (savedChores) {
+    choresArray = JSON.parse(choresArrey);
+  }
+}
+
+addButtonElement.addEventListener("click", addChores);
+
+// function to push input to array and store array in local storage
+function addChores() {
+  const chore = {
+    text: inputElement.value,
+    choreDone: false,
+  };
+
+  choresArray.push(chore);
+  localStorage.setItem("choresArray", JSON.stringify(choresArray));
+
+  displayChores();
+
+  // To clear the inputfield
+  inputElement.value = "";
+}
+
+function displayChores() {
+  const chore = {
+    text: inputElement.value,
+    choreDone: false,
+  };
+
+  //   prevents tasks from multiplying
+  choresElement.innerHTML = "";
+
+  if (inputElement.value.length > 0) {
+    for (let chores of choresArray) {
+      const choreBuldingBlocks = document.createElement("div");
+
+      const inputChore = document.createElement("span");
+      inputChore.innerHTML = chore.text;
+      inputChore.classList.add("text");
+      choreBuldingBlocks.appendChild(inputChore);
+
+      const doneButton = document.createElement("button");
+      doneButton.innerText = "✓";
+      choreBuldingBlocks.appendChild(doneButton);
+
+      const deleteButton = document.createElement("button");
+      deleteButton.innerText = "✕";
+      choreBuldingBlocks.appendChild(deleteButton);
+
+      choresElement.appendChild(choreBuldingBlocks);
+
+      doneButton.addEventListener("click", () => {
+        chore.choreDone = true;
+        inputChore.style.textDecoration = "overline";
+
+        localStorage.setItem("choresArray", JSON.stringify(choresArray));
+        displayChores();
+      });
+
+      deleteButton.addEventListener("click", () => {
+        const choresIndex = choresArray.indexOf(chore);
+        choresArray.splice(choresIndex, 1);
+
+        localStorage.setItem("choresArray", JSON.stringify(choresArray));
+        displayChores();
+      });
+    }
+  }
+}
--- toDoList.js
+++ toDoList.js
@@ -1,14 +1,35 @@
-const listElement = document.getElementById("list");
 const inputElement = document.getElementById("input");
 const addButtonElement = document.getElementById("add");
 const choresElement = document.getElementById("chores");
 const completedElement = document.getElementById("completed");
 
+const choresArray = [];
+console.log(choresArray);
+
+function save(){
+    localStorage.saveChores = JSON.stringify(choresArray)
+}
+
+function refreshList(){
+    choresElement.innerHTML = ""
+    completedElement.innerHTML = ""
+
+    for(let chores in choresArray){
+
+    }
+}
+
 addButtonElement.addEventListener("click", addChores);
 
-function addChores() {
+function addChores(chores) {
   if (inputElement.value.length > 0) {
     // console.log(inputElement.value);
+    const item = {
+        text: "inputChore"
+        choreDone: false
+    }
+
+    // The text
     const inputChore = document.createElement("div");
     inputChore.innerText = inputElement.value;
     inputChore.classList.add("text");
@@ -16,6 +37,7 @@ function addChores() {
 
     inputElement.value = "";
 
+    // The button for a completed task
     const doneButton = document.createElement("button");
     doneButton.innerText = "✓";
     choresElement.appendChild(doneButton);
@@ -25,6 +47,7 @@ function addChores() {
       completedElement.appendChild(deleteButton);
     });
 
+    // The button to delete a task
     const deleteButton = document.createElement("button");
     deleteButton.innerText = "✕";
     choresElement.appendChild(deleteButton);
